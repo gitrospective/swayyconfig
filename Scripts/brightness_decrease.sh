93e669921fb52b996f1021b5a17353b2ed8affa3
gitrospective <126268696+gitrospective@users.noreply.github.com>
2023-06-14 20:32:59 +0000

    Update brightness_decrease.sh
    comments

--- Scripts/brightness_decrease.sh
+++ Scripts/brightness_decrease.sh
@@ -5,3 +5,5 @@ new_brightness=$(awk '{brightness = $1 - 100; if (brightness < 0) brightness = 0
 
 brightnessctl -d intel_backlight s "$new_brightness"
 
+# while intel_backlight is a common for most systems it could be different depending on your hardware
+#check /sys/class/backlight directory for further information
