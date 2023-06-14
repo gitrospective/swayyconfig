2585c7f334fdb0050a95dc51a1bdac428db22754
gitrospective <126268696+gitrospective@users.noreply.github.com>
2023-06-14 20:04:51 +0000

    Update battery.sh
    added the code to make it function

--- Scripts/battery.sh
+++ Scripts/battery.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+threshold=20  #you can set it to whichever percent you prefer
+while true; do
 
+	battery_level=$(acpi | awk '{print $4}' | tr -d ',' | tr -d '%')
+	battery_status=$(acpi | awk '{print $3}' | tr -d ',')
+
+	if [[ $battery_level -le $threshold && $battery_status = "Discharging" ]]; then
+		dunstify -u critical "Battery Low" "Battery level is at ${battery_level}%!"
+	fi
+	sleep 5
+done
